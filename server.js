b26328953d0e502a5f83542317f4161dd4eaa59c
Kaitlyn Rubbo <kaitlynrubbo@gmail.com>
2021-03-10 20:12:39 -0500

    working on local

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 3310;
 
 const app = express();
 
